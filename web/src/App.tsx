c39a412871118971a272722ae254385bc0a71d53
Marcos Felipe <p.marcosfelipe@gmail.com>
2023-03-07 21:05:22 +0000

    start web layout using TailwindCSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,19 @@
+import "./styles/main.css";
+
+import logoImg from "./assets/logo-nlw.svg";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img alt="Logo Image" src={logoImg} />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Your{" "}
+        <span className="bg-gradient text-transparent bg-clip-text">duo</span>{" "}
+        is here
+      </h1>
+    </div>
+  );
 }
 
 export default App;
